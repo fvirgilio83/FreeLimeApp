d513f4bf9814f66877bb7b6f35f9b5b54d05ccee
ofernandovirgilio <63621171+fvirgilio83@users.noreply.github.com>
2024-03-01 15:40:16 -0300

    Update 3_📈_Plots.py

--- pages/3_📈_Plots.py
+++ pages/3_📈_Plots.py
@@ -11,21 +11,21 @@ st.title("FreeLime Prediction x FreeLime Real")
 st.markdown("Trend based on the last 20 values:")
 
 
-    c= [i for i in range(1,21,1)]
-    fig, ax =plt.subplots(figsize=(10,8), layout='constrained')
+c= [i for i in range(1,21,1)]
+fig, ax =plt.subplots(figsize=(10,8), layout='constrained')
 
 
-    x1= grafico.iloc[grafico.shape[0]-20:grafico.shape[0]+1,-2]
-    x2 = grafico.iloc[grafico.shape[0]-20:grafico.shape[0]+1,-1]
+x1= grafico.iloc[grafico.shape[0]-20:grafico.shape[0]+1,-2]
+x2 = grafico.iloc[grafico.shape[0]-20:grafico.shape[0]+1,-1]
 
 
-    ax.scatter(c,x1,facecolor = 'C1', edgecolor= 'k', label='FreeLime_Real')
-    ax.plot(c, x2, 'o--', label='FreeLime_Predicted')
-    ax.set_ylabel(' Free Lime ')
-    ax.set_xlabel(' Last 20 results ')
-    ax.legend()
+ax.scatter(c,x1,facecolor = 'C1', edgecolor= 'k', label='FreeLime_Real')
+ax.plot(c, x2, 'o--', label='FreeLime_Predicted')
+ax.set_ylabel(' Free Lime ')
+ax.set_xlabel(' Last 20 results ')
+ax.legend()
 
 
-    st.pyplot(fig)
+st.pyplot(fig)
    
 
